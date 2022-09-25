788a878333e4dfc45bdf2ac885b460b9497671eb
noah-brunate <noahbrunate9@gmail.com>
2022-09-25 06:40:22 -0700

    pytgon data structures

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,8 @@
 
 def element_at(my_list, idx):
     for i in my_list:
-        if i == idx:
-            if idx < 0 and idx > len(my_list):
-                return none
-            else:
-                return idx
-            print()
+        if idx < 0 and idx > len(my_list):
+            return none
+        else:
+            return my_list[idx]
+        print()
