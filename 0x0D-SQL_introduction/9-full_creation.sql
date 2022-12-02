e8a8484e813090ca3d2f0458de9502722b0289b2
noah-brunate <noahbrunate9@gmail.com>
2022-12-02 12:03:33 -0800

    sql scripts

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,6 +2,6 @@
 
 CREATE TABLE IF NOT EXITS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score)
-VALUES (1, "John", 10), (2, "John", 3), (3, "Bob", 14), (4, "George", 18);
+VALUES (1, "John", 10), (2, "John", 3), (3, "Bob", 14), (4, "George", 8);
 
 
