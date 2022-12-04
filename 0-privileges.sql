b1856839796fa9edd11917ed850c79dd84bb321d
noah-brunate <noahbrunate9@gmail.com>
2022-12-04 06:44:35 -0800

    mysql subqueries

---
+++ 0-privileges.sql
@@ -0,0 +1,6 @@
+--script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+
+SHOW GRANTS 'user_0d_1'@'localhost'
+SHOW GRANTS 'user_0d_2'@'localhost'
+
+
