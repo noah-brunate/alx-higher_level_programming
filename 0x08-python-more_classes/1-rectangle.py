38d687d87a3a57161da7affbed359bb2512d99ee
noah-brunate <noahbrunate9@gmail.com>
2023-01-04 06:57:48 -0800

    python more classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,8 +7,19 @@ class Rectagle:
     """Class represent a rectagle"""
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
+
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = value
 
     @property
     def width(self):
