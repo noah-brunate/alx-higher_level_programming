f1ff63b00b5212dadce41c6a9e4aefe6aca96b31
noah-brunate <noahbrunate9@gmail.com>
2023-03-08 13:45:49 -0800

    more coditionals

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def remove_char_at(str, n):
+    if n < 0:
+        return str
+    if n > len(str):
+        return str
     nstr = ''
     for i in range(0, len(str)):
         if str[i] == str[n]:
