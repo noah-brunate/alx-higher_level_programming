8bae8323ce5cc3e4dd2470188aa6a44648db8b9a
noah-brunate <noahbrunate9@gmail.com>
2023-03-13 15:02:10 -0700

    python data structures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        for j in i:
-            print('{}'.format(j))
+    for row in matrix:
+        for j in row:
+            if i is not row[len(row) - 1]:
+                print("{:d}".format(i), end=" ")
+            else:
+                print("{:d}".format(i), end=" ")
+    print()
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    i = 0
-    if len(tuple_a) == i:
-        if len(tuple_b) == i:
-            return (0, 0)
-        elif len(tuple_b) == 1:
-            return (tuple_b[i], 0)
+"""Add two tuples."""
+
+
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = 0, 0
         else:
-            return (tuple_b[i], tuple_b[1]
-    if len(tuple_b) == i:
-        if len(tuple_a) == 1:
-            return (tuple_a[i], 0)
+            tuple_a = tuple_a[0], 0
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
         else:
-            return (tuple_a[i], tuple_a[1])
-    if len(tuple_a) == 1:
-        if len(tuple_b) == 1:
-            return (tuple_a[i] + tuple_a[i], 0)
-        else:
-            return (tuple_a[i] + tuple_b[i], tuple_b[1])
-    if len(tuple_b) == 1:
-        if len(tuple_a) > 1:
-            return (tuple_a[i] + tuple_b[i], tuple_a[1])
-    new_tuple = (tuple_a[i] + tuple_b[i], tuple_a[1] + tuple_b[1])
-    return new_tuple
+            tuple_b = tuple_b[0], 0
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
