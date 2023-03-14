0e87a60e1158469278389f773450c5a27692d2e5
noah-brunate <noahbrunate9@gmail.com>
2023-03-14 11:17:06 -0700

    python data structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    str = list(filter(lambda n: n != 'c' or n != 'C', my_string))
+    str = [n for n in my_string if n != 'c' and n != 'C']
     n_str = "".join(str)
     return n_str
