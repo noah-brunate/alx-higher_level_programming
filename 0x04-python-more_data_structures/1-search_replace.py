58083e322960a0dafa744792d9ccbdf229cec358
noah-brunate <noahbrunate9@gmail.com>
2023-03-15 09:18:11 -0700

    python more data structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 
 def search_replace(my_list, search, replace):
     for i in my_list:
-        if i = search:
-        fd = my_list.index(search)
-        my_list[fd] = replace
+        if i == search:
+            fd = my_list.index(search)
+            my_list[fd] = replace
     return my_list
