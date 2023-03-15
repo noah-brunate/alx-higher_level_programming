38a6caa1b42477897c71c32ebc28dacf99a69aae
noah-brunate <noahbrunate9@gmail.com>
2023-03-15 06:25:16 -0700

    python more data structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-    for i in range(2):
-        for j in new_matrix[i]:
-            j = j**2
+    new_matrix = [x ** 2 for x in matrix]
     return new_matrix
