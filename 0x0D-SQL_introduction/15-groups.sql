9fcf2410c58213a27286a749e0c671262b540c58
noah-brunate <noahbrunate9@gmail.com>
2023-05-19 08:18:35 -0700

    more queries

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,7 +1,5 @@
 -- script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
 
-SELECT DISTINCT score, COUNT(*) AS number
-FROM second_table
-ORDER BY number DESC;
+SELECT DISTINCT score, COUNT(*) AS number FROM second_table ORDER BY number DESC;
 
 
