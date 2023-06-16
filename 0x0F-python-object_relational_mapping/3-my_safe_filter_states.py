7934c9e745c04c212e058e0ecbad7e437794765e
noah-brunate <noahbrunate9@gmail.com>
2023-06-16 13:53:33 -0700

    python-database

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""script prevents sql injection through user input"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    detects sql injection
+    """
+    sql = "SELECT * FROM states WHERE name = %s\
+        ORDER BY states.id ASC"
+
+    conn == MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306,
+        passwd=argv[2], db=argv[3])
+
+    cursor = conn.cursor()
+
+    cursor.execute(sql, (argv[4]))
+
+    results = cursor.fetchall()
+    for val in results:
+        print(val)
