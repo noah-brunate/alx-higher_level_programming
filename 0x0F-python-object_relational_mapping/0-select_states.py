80ac922b81734fd4a24b277f10ac4509ab094315
noah-brunate <noahbrunate9@gmail.com>
2023-06-16 06:27:04 -0700

    python-databases

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    """
+    retrieves states from the database
+    """
+
+    conn = MySQLdb.connect(
+        host="localhost", port=3306, user=sys.argv[1],
+        passwd=sys.argv[2], db=sys.argv[3])
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM states")
+
+    results = cursor.fetchall()
+
+    for num in results:
+        print(num)
