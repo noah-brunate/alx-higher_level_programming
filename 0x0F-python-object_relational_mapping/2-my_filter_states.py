d787ec7d807dac10cb2a86297e6dd694cadf8513
noah-brunate <noahbrunate9@gmail.com>
2023-06-16 07:35:01 -0700

    pyrhon-databases

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-"""script that lists all states with a name starting with N hbtn_0e_0_usa"""
+"""script displays values in the states table of hbtn_0e_0_usa"""
 
 import MySQLdb
 from sys import argv
 
-
 if __name__ == '__main__':
     """
-    queries the database for states starting with N
+    displays values where name matches the argument
     """
 
     conn = MySQLdb.connect(
         host="localhost", user=argv[1], port=3306,
-        passwd=argv[2], db=argb[3])
+        passwd=argv[2], db=argv[3])
 
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
-        ORDER BY states.id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+        ORDER BY states.id ASC".format(argv[4]))
 
     results = cursor.fetchall()
-
     for val in results:
         print(val)
