ec9f2f83ca38cfa9100c408576fc5afd5ef60072
noah-brunate <noahbrunate9@gmail.com>
2023-06-16 07:37:41 -0700

    pyrhon-databases

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 
     conn = MySQLdb.connect(
         host="localhost", user=argv[1], port=3306,
-        passwd=argv[2], db=argb[3])
+        passwd=argv[2], db=argv[3])
 
     cursor = conn.cursor()
 
