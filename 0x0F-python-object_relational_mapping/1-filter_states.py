2b2c045a86f86e07e3d2ec82fc417bcb1f2f6d5b
noah-brunate <noahbrunate9@gmail.com>
2023-06-16 07:01:30 -0700

    pyton-databases

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!usr/bin/python3
+"""script that lists all states with a name starting with N hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    """
+    queries the database for states starting with N
+    """
+
+    conn = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306,
+        passwd=argv[2], db=argb[3])
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name=N*")
+
+    results = cursor.fetchall()
+
+    for val in results:
+        print(val)
