48515325d837c73ef00345b9e638e8bb617ea25a
noah-brunate <noahbrunate9@gmail.com>
2023-07-25 14:25:25 -0700

    web

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const filename = process.argv[2];
 
-fs.readFile(filename, 'utf-8', (error, content)) => {
+fs.readFile(filename, 'utf-8', (error, content) => {
   if (error) {
     console.log(error);
   } else {
