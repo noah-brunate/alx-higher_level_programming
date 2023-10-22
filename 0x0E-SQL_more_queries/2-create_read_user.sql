8e9153aebe26cee1c45b17834331337d8354e828
noah-brunate <noahbrunate9@gmail.com>
2023-10-22 04:23:27 -0700

    sql codes

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- script creates a database hbtn_0d_2 and
--- user hbtn_0d_2@localhost with SELECT privileges
+-- user user_0d_2@localhost with SELECT privileges
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'htbn_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON `hbtn_0d_2`.* TO 'hbtn_0d_2'@'localhost';
-FLUSH PRIVILEGES
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
