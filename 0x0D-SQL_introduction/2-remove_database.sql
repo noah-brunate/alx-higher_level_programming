08d4bc3d3336c1a8ed0b737d868fa9346f404e66
noah-brunate <noahbrunate9@gmail.com>
2023-10-22 03:17:09 -0700

    mysql code rectify

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
--- script that deletes the database hbtn_0c_0 in your MySQL server
+-- script deletes the database hbtn_0c_0 in your MySQL server
 
-DROP DATABASE IF EXISTS 'hbtn_0c_0';
+DROP DATABASE IF EXISTS hbtn_0c_0;
