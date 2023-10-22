e5bd6e3945abf0e7001d84bde28d2e1a0aa9e4c9
noah-brunate <noahbrunate9@gmail.com>
2023-10-22 04:15:31 -0700

    mysql code remedy

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,4 @@
---script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+-- script shows privileges for 'user_0d_1'@'localhost' and 'user_0d_2'@'localhost'
 
-SHOW GRANTS 'user_0d_1'@'localhost'
-SHOW GRANTS 'user_0d_2'@'localhost'
-
-
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
