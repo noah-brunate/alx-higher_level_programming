b895f9be265b8a081ccb5c8f5cdfabf0bd9c7fb1
noah-brunate <noahbrunate9@gmail.com>
2023-10-22 04:19:58 -0700

    sql code

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- user hbtn_0d_2@localhost with SELECT privileges
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'htbn_0d_2'@'localhost' IDENTIFIED BY 'user_pwd';
+CREATE USER IF NOT EXISTS 'htbn_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON `hbtn_0d_2`.* TO 'hbtn_0d_2'@'localhost';
 FLUSH PRIVILEGES
